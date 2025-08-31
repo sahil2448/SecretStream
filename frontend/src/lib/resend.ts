6c88f9cc8a52481f4d7e9a125010dcaaf6c17a69
Sahil Kamble <syk2448@gmail.com>
2025-08-31 15:57:18 +0530

    pushing for production

--- frontend/src/lib/resend.ts
+++ frontend/src/lib/resend.ts
@@ -1,6 +1,3 @@
-import dotenv from "dotenv"
-dotenv.config();
 import { Resend } from 'resend';
 
-console.log("resend api key", process.env.RESEND_API_KEY)
-export const resend = new Resend(process.env.RESEND_API_KEY!);
+export const resend = new Resend(process.env.RESEND_API_KEY as string);
